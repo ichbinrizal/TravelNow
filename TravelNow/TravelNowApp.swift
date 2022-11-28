5ea0e7daaf91342e2766597df6f30a3a0a961236
ichbinrizal <rizal.saputro@tiket.com>
2022-11-28 16:47:37 +0700

    Initial Commit

---
+++ TravelNow/TravelNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelNowApp.swift
+//  TravelNow
+//
+//  Created by Rizal on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TravelNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
