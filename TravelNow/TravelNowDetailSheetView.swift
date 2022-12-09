dde0fa5a83f7949249e24a84ccabcad8f769dcbf
Rizal <rizal.es@hotmail.com>
2022-12-09 17:28:37 +0700

    first commit

---
+++ TravelNow/TravelNowDetailSheetView.swift
@@ -0,0 +1,20 @@
+//
+//  TravelNowSheetView.swift
+//  TravelNow
+//
+//  Created by Rizal on 28/11/22.
+//
+
+import SwiftUI
+
+struct TravelNowDetailSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TravelNowSheetView_Previews: PreviewProvider {
+    static var previews: some View {
+        TravelNowDetailSheetView()
+    }
+}
---
+++ TravelNow/TravelNowWebView.swift
@@ -0,0 +1,8 @@
+//
+//  TravelNowWebView.swift
+//  TravelNow
+//
+//  Created by Rizal on 29/11/22.
+//
+
+import Foundation
